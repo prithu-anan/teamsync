fe28905902c86fb40d70ff74f8259ce964b86b0a
Prithu Anan <prithua00@gmail.com>
2025-08-13 01:20:38 +0600

    fix: health endpoint for auth

--- api-gateway/src/main/java/com/teamsync/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/teamsync/api_gateway/routes/Routes.java
@@ -12,17 +12,24 @@ import org.springframework.web.servlet.function.ServerResponse;
 public class Routes {
 
     @Bean
-    public RouterFunction<ServerResponse> userManagementService() {
-        return GatewayRouterFunctions.route("user_management_service")
-                .route(RequestPredicates.path("/api/users/**"), HandlerFunctions.http("http://user-management-service:8082"))
-                .build();
-    }
-
-    @Bean
     public RouterFunction<ServerResponse> testRoute() {
         return GatewayRouterFunctions.route()
                 .route(RequestPredicates.GET("/health"),
                         request -> ServerResponse.ok().body("Gateway is alive!"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> authService() {
+        return GatewayRouterFunctions.route("auth_service")
+                .route(RequestPredicates.path("/auth/**"), HandlerFunctions.http("http://auth-service:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userManagementService() {
+        return GatewayRouterFunctions.route("user_management_service")
+                .route(RequestPredicates.path("/api/users/**"), HandlerFunctions.http("http://user-management-service:8082"))
                 .build();
     }
 
--- auth-service/src/main/java/com/teamsync/auth/controller/HealthController.java
+++ auth-service/src/main/java/com/teamsync/auth/controller/HealthController.java
@@ -2,12 +2,14 @@ package com.teamsync.auth.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class HealthController {
 
     @GetMapping("/health")
