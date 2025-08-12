e4d4279b020327ad95b0a35ae66206397c7af2ba
Prithu Anan <prithua00@gmail.com>
2025-08-13 00:04:28 +0600

    added api gateway

---
+++ api-gateway/src/main/java/com/teamsync/api_gateway/routes/Routes.java
@@ -0,0 +1,29 @@
+package com.teamsync.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> userManagementService() {
+        return GatewayRouterFunctions.route("user_management_service")
+                .route(RequestPredicates.path("/api/users/**"), HandlerFunctions.http("http://user-management-service:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> testRoute() {
+        return GatewayRouterFunctions.route()
+                .route(RequestPredicates.GET("/health"),
+                        request -> ServerResponse.ok().body("Gateway is alive!"))
+                .build();
+    }
+
+}
